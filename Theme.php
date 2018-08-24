96569cd2c2d9e343d155ab78ee50be5ff8913a00
Rodrigo Guimarães <rodrigodeoliveira@hotmail.com.br>
2018-08-24 15:10:46 -0300

    Finishes first part of theme's customizations (#207)

--- Theme.php
+++ Theme.php
@@ -3,6 +3,7 @@ namespace PontosMemoria;
 
 use BaseMinc;
 use MapasCulturais\App;
+use MapasCulturais\i;
 
 class Theme extends BaseMinc\Theme {
     public function _init() {
@@ -26,8 +27,13 @@ class Theme extends BaseMinc\Theme {
 
     public function getMetaConfig($meta,$cfg)
     {
-        if ( array_key_exists($meta, $this->_getSpaceMetadata()) ) {
-            return $this->_getSpaceMetadata()[$meta][$cfg];
+        if (array_key_exists($meta, $this->_getSpaceMetadata())) {
+            $value = "";
+            if (isset($this->_getSpaceMetadata()[$meta][$cfg])) {
+                $value = $this->_getSpaceMetadata()[$meta][$cfg];
+            }
+
+            return $value;
         }
     }
 
@@ -63,27 +69,92 @@ class Theme extends BaseMinc\Theme {
                 'validations' => [
                     'required' => \MapasCulturais\i::__('Favor informar se este Ponto de Memória possui Conselho Gestor.')
                 ]
-            ]
+            ],
+
+            'tematica_ponto_memoria' => [
+                'label' => 'Temática dos Pontos de Memória',
+                'type' => 'select',
+                'options' => [
+                    'Acervo e centro de memória',
+                    'Conhecimento e tradições orais',
+                    'Povos e comunidades tradicionais',
+                    'Culturas Afro-brasileiras',
+                    'Culturas Indígenas',
+                    'Culturas Populares',
+                    'Cultura Cigana',
+                    'Cultura LGBT',
+                    'Cultura Digital',
+                    'Cultura Estrangeira',
+                    'Patrimônio Imaterial',
+                    'Educação patrimonial',
+                    'Turismo Comunitário',
+                    'Pesquisa em Memória Social',
+                    'Mídias Comunitárias',
+                    'Arquivo/Arquivo Digital',
+                    'Produção Cultural',
+                    'Audiovisual',
+                    'Artes visuais',
+                    'Arte de rua',
+                    'Rádio Comunitária',
+                    'Televisão Comunitária',
+                    'Economia Criativa',
+                    'Artesanato',
+                    'Circo',
+                    'Dança',
+                    'Música',
+                    'Cinema',
+                    'Fotografia',
+                    'Teatro',
+                    'Literatura',
+                ],
+                'validations' => [
+                    'required' => \MapasCulturais\i::__('É obrigatório informar a temática do ponto de memória')
+                ]
+            ],
+
         ];
     }
 
     public function renderAllMetas($entity)
     {
         foreach ($this->_getSpaceMetadata() as $key => $data) {
-            $label = $this->getMetaConfig($key, 'label');
-            $desc = $this->getMetaConfig($key, 'description');
-            ?>
-            <p class="privado">
-                <span class="label required"> <?php echo $label; ?></span>
-                <?php if ($this->isEditable() || $entity->getMetadata()[$key]): ?>
-                    <editable-singleselect entity-property="<?php echo $key; ?>" empty-label="Selecione" 
-                        allow-other="true" box-title="<?php echo $label; ?>"></editable-singleselect>
-                <?php endif; ?>
-            </p>
-            <p style="padding-left: 10px; font-style: italic;margin-bottom: 10px;">
-                <?php echo $desc; ?>
-            </p>
-        <?php 
+            $tipo = $this->getMetaConfig($key,'type');
+            if ("select" === $tipo) {
+                $label = $this->getMetaConfig($key, 'label');
+                $desc = $this->getMetaConfig($key, 'description');
+                ?>
+                <p class="privado">
+                    <span class="label required"> <?php echo $label; ?></span>
+                    <?php if ($this->isEditable() || $entity->getMetadata()[$key]): ?>
+                        <editable-singleselect entity-property="<?php echo $key; ?>" empty-label="Selecione"
+                                               allow-other="true" box-title="<?php echo $label; ?>"></editable-singleselect>
+                    <?php endif; ?>
+                </p>
+                <p class="descricao-meta-ponto-memoria"> <?php echo $desc; ?> </p>
+                <?php
+            }
         }
+    }
+
+    protected function _getFilters()
+    {
+        $filters = parent::_getFilters();
+        $filters['space'] = [
+            [
+                'label' => 'Temática dos Pontos de Memoria',
+                'placeholder' => 'Selecione as Temáticas',
+                'filter' => [
+                    'param' => 'tematica_ponto_memoria',
+                    'value' => 'IN({val})'
+                ],
+                'validations' => [
+                    'required' => 'Preencher a temática'
+                ]
+            ]
+        ];
+
+        App::i()->applyHookBoundTo($this, 'search.filters', [&$filters]);
+
+        return $filters;
     }
 }
