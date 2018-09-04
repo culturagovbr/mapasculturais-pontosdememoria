d5f07011e5326661b13cb008f1e55f2cca0bdf76
Rodrigo Guimarães <rodrigodeoliveira@hotmail.com.br>
2018-09-04 14:54:28 -0300

    torna estado um metadado obrigatorio

--- space-types.php
+++ space-types.php
@@ -2,6 +2,42 @@
 
 $core_space_types = include APPLICATION_PATH.'/conf/space-types.php';
 
+$core_space_types['metadata']['En_Estado'] = array(
+    'label' => \MapasCulturais\i::__('Estado'),
+    'type' => 'select',
+    'options' => array(
+        'AC'=>'Acre',
+        'AL'=>'Alagoas',
+        'AP'=>'Amapá',
+        'AM'=>'Amazonas',
+        'BA'=>'Bahia',
+        'CE'=>'Ceará',
+        'DF'=>'Distrito Federal',
+        'ES'=>'Espírito Santo',
+        'GO'=>'Goiás',
+        'MA'=>'Maranhão',
+        'MT'=>'Mato Grosso',
+        'MS'=>'Mato Grosso do Sul',
+        'MG'=>'Minas Gerais',
+        'PA'=>'Pará',
+        'PB'=>'Paraíba',
+        'PR'=>'Paraná',
+        'PE'=>'Pernambuco',
+        'PI'=>'Piauí',
+        'RJ'=>'Rio de Janeiro',
+        'RN'=>'Rio Grande do Norte',
+        'RS'=>'Rio Grande do Sul',
+        'RO'=>'Rondônia',
+        'RR'=>'Roraima',
+        'SC'=>'Santa Catarina',
+        'SP'=>'São Paulo',
+        'SE'=>'Sergipe',
+        'TO'=>'Tocantins',
+    ),
+    'validations' => array(
+        'required' => \MapasCulturais\i::__('O estado é obrigatório')
+    )
+);
 $core_space_types["items"]["PontosMemoria"] = [
     'range' => array(130,136),
     'items' => [
